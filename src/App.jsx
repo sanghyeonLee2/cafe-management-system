2e13626ef7f4e3eb12fc034b70ed3600531a9de4
sanghyeonLee2 <hyeon012366@gmail.com>
2023-11-12 04:10:39 +0900

    refactor : 코드 가독성과 길이 단축 및 서버 포트번호, 엔드 포인트 수정에 따른 요청URL 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import Header from "./component/Header";
 import styled from "styled-components";
 import {Outlet} from "react-router-dom";
 import Signin from "./component/Signin";
-import { useRecoilValue, useSetRecoilState} from "recoil";
+import {useRecoilValue, useSetRecoilState} from "recoil";
 import {SigninPopupAtom, SignupPopupAtom} from "./recoil/PopupAtom";
 import Signup from "./component/Signup";
 import {useEffect} from "react";
 import axios from "axios";
 import {UserIdAtom, UserInfoAtom} from "./recoil/UserAuth";
 import {MenuItemAtom} from "./recoil/MenuItemAtom";
-import Supplier from "./pages/Supplier";
 
 function App() {
     const userIdState = useRecoilValue(UserIdAtom)
@@ -20,36 +19,33 @@ function App() {
     useEffect(() => {
             const fetchUserInfo = async () => {
                 const result = await
-                    axios.get("http://localhost:4000/signin/success")
-                if (result.data.user) {
-                    console.log(result.data.user)
-                    setUserInfo({
-                        address: result.data.user.userAddress,
-                        username: result.data.user.username,
-                        phoneNum: result.data.user.userPhoneNum
-                    });
-                } else if (result.status === 404) {
-                    console.log("실패")
+                    axios.get("http://localhost:8080/user/signin/success", {withCredentials: true})
+                if (result.status === 200) {
+                    setUserInfo(result.data);//userId는 어따씀?
+                    return;
+                }
+                if (result.status === 404) {
                     userIdState(null);
                 }
             }
             fetchUserInfo();
         }
-        , [setUserInfo,userIdState]);
-
-    useEffect(() => {
-            const fetchMenuItem = async () => {
-                const result = await
-                    axios.get("http://localhost:4000/get-menu")
-                setMenuItem(result.data);
-                if (result.status === 200) {
-                } else {
+        , [setUserInfo, userIdState]);
+        useEffect(() => {
+                const fetchMenuItem = async () => {
+                    const result = await
+                        axios.get("http://localhost:8080/menu-item")
+                    if (result.status === 200) {
+                        setMenuItem(result.data);
+                        return
+                    }
                     alert("메뉴를 불러오지 못했습니다.")
                 }
+                fetchMenuItem();
             }
-            fetchMenuItem();
-        }
-        , [setMenuItem]);
+            , []);
+
+
 
     const signinPopupValue = useRecoilValue(SigninPopupAtom)
     const signupPopupValue = useRecoilValue(SignupPopupAtom)
