811b4addced902235ed798d2dd621eecf5f02332
SanghyeonLee <hyeon012366@gmail.com>
2023-11-16 17:40:00 +0900

    feat: 헤더에 관리자 전용 메뉴 및 주문 내역 링크 추가

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,42 +1,59 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import {useRecoilValue, useSetRecoilState} from "recoil";
-import {SigninPopupAtom, SignupPopupAtom} from "../recoil/PopupAtom";
-import {UserIdAtom} from "../recoil/UserAuth";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { SigninPopupAtom, SignupPopupAtom } from "../recoil/PopupAtom";
+import { UserIdAtom, UserInfoAtom } from "../recoil/UserAuth";
 import SignoutBtn from "./SignoutBtn";
-import {useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
-    const userIdValue = useRecoilValue(UserIdAtom)
-    const setSigninPopup = useSetRecoilState(SigninPopupAtom);
-    const setSignupPopup = useSetRecoilState(SignupPopupAtom)
-    const navigate = useNavigate(-1)
-    return (
-        <HeaderLayout>
-            {userIdValue ? <SignoutBtn/> :
-                <div>
-                    <p onClick={() => setSignupPopup(true)}>회원가입</p>
-                    <p onClick={() => setSigninPopup(true)}>로그인</p>
-                </div>
-            }
-            <div><p onClick={() => navigate(-1)}> 뒤로가기</p></div>
-            <TitleLayout>Coffee</TitleLayout>
-        </HeaderLayout>
-    );
+  const userIdValue = useRecoilValue(UserIdAtom);
+  const userInfoAtom = useRecoilValue(UserInfoAtom);
+  const setSigninPopup = useSetRecoilState(SigninPopupAtom);
+  const setSignupPopup = useSetRecoilState(SignupPopupAtom);
+  const navigate = useNavigate(-1);
+  return (
+    <HeaderLayout>
+      {userIdValue ? (
+        <div>
+          <SignoutBtn />
+          {userInfoAtom.userIsAdmin && (
+            <p>
+              <Link to={"/admin"}>관리자 페이지</Link>
+            </p>
+          )}
+          <Link to={"/order"}>
+            <p>나의 주문 내역</p>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p onClick={() => setSignupPopup(true)}>회원가입</p>
+          <p onClick={() => setSigninPopup(true)}>로그인</p>
+        </div>
+      )}
+      <div>
+        <p onClick={() => navigate(-1)}> 뒤로가기</p>
+      </div>
+      <Link to={"/"}>
+        <TitleLayout>Coffee</TitleLayout>
+      </Link>
+    </HeaderLayout>
+  );
 }
 
 const HeaderLayout = styled.div`
   p {
     display: inline-block;
     cursor: pointer;
-    margin-left: 20px
+    margin-left: 20px;
   }
-`
+`;
 
 const TitleLayout = styled.h1`
   display: flex;
   margin: 40px 450px;
   justify-content: center;
   align-items: center;
-`
+`;
 export default Header;
