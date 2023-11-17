8909d53f372e6cc01a06d0c223758603727933ea
SanghyeonLee <hyeon012366@gmail.com>
2023-11-17 18:40:00 +0900

    style: 관리자 페이지 메뉴 항목 정렬 및 텍스트 정리

--- src/pages/admin/AdminPage.jsx
+++ src/pages/admin/AdminPage.jsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function AdminPage(props) {
-    return (
-        <>
-            <AdminTitleLayout>
-                관리자 페이지
-            </AdminTitleLayout>
-            <ul>
-                <li><Link to={"/admin/menu-management"}>메뉴관리</Link></li>
-                <li><Link to={"/admin/user-management"}>고객관리</Link></li>
-                <li><Link to={"/admin/order-management"}>주문관리</Link></li>
-                <li><Link to={"/admin/material-management"}>재료관리/메뉴생성</Link></li>
-                <li><Link to={"/admin/supplier-management"}>공급업체 목록</Link></li>
-            </ul>
-        </>
-    );
+  return (
+    <>
+      <AdminTitleLayout>관리자 페이지</AdminTitleLayout>
+      <ul>
+        <PageLi>
+          <Link to={"/admin/menu-management"}>메뉴관리</Link>
+        </PageLi>
+        <PageLi>
+          <Link to={"/admin/user-management"}>고객관리</Link>
+        </PageLi>
+        <PageLi>
+          <Link to={"/admin/order-management"}>주문관리</Link>
+        </PageLi>
+        <PageLi>
+          <Link to={"/admin/material-management"}>재료관리</Link>
+        </PageLi>
+        <PageLi>
+          <Link to={"/admin/supplier-management"}>공급업체 목록</Link>
+        </PageLi>
+      </ul>
+    </>
+  );
 }
 
 const AdminTitleLayout = styled.h2`
   display: flex;
   justify-content: center;
-`
+`;
+const PageLi = styled.li`
+  display: flex;
+  justify-content: center;
+`;
+
 export default AdminPage;
