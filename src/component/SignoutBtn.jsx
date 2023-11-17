7952cce2fc3cdebaeef240c2d2dfc68dab0d1fe3
SanghyeonLee <hyeon012366@gmail.com>
2023-11-17 11:40:00 +0900

    refactor: SignoutBtn 불필요한 div 제거

--- src/component/SignoutBtn.jsx
+++ src/component/SignoutBtn.jsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import {useSetRecoilState} from "recoil";
-import {UserIdAtom} from "../recoil/UserAuth";
-import {useNavigate} from "react-router-dom";
-import {OrderListAtom} from "../recoil/OrderListAtom";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { UserIdAtom } from "../recoil/UserAuth";
+import { useNavigate } from "react-router-dom";
+import { OrderListAtom } from "../recoil/OrderListAtom";
 
 function SignoutBtn(props) {
-    const navigate = useNavigate() ;
-    const userIdState = useSetRecoilState(UserIdAtom);
-    const setOrderListState = useSetRecoilState(OrderListAtom);
-    const signoutBtn = () => {
-        userIdState(null);
-        setOrderListState([]);
-        navigate("/");
-    }
-    return (
-        <div><p onClick={signoutBtn}>로그아웃</p></div>
-    );
+  const navigate = useNavigate();
+  const userIdState = useSetRecoilState(UserIdAtom);
+  const setOrderListState = useSetRecoilState(OrderListAtom);
+  const signoutBtn = () => {
+    userIdState(null);
+    setOrderListState([]);
+    navigate("/");
+  };
+  return <p onClick={signoutBtn}>로그아웃</p>;
 }
 
 export default SignoutBtn;
