4768f60725b6841b555915526a41d157d121e2ef
SanghyeonLee <hyeon012366@gmail.com>
2023-11-20 17:40:00 +0900

    feat: react-router-dom v6 router 구성 및 주요 페이지 라우팅 설정 추가

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
-import {RecoilRoot} from "recoil";
+import { RecoilRoot } from "recoil";
 import AdminPage from "./pages/admin/AdminPage";
 import MenuManagement from "./pages/admin/MenuManagement";
 import UserManagement from "./pages/admin/UserManagement";
@@ -12,54 +12,70 @@ import MaterialManagement from "./pages/admin/MaterialManagement";
 import SupplierManagement from "./pages/admin/SupplierManagement";
 import Supplier from "./pages/Supplier";
 import MenuItemInfo from "./pages/MenuItemInfo";
+import Order from "./pages/Order";
+import OrderInfo from "./pages/OrderInfo";
 
-const router = createBrowserRouter([{
+const router = createBrowserRouter([
+  {
     path: "/",
-    element: <App/>,
+    element: <App />,
     children: [
-        {
-            path: "",
-            element: <Home/>
-        },
-        {
-            path: "/admin",
-            element: <AdminPage/>,
-        },
-        {
-            path: "/admin/menu-management",
-            element: <MenuManagement/>
-        }
-        , {
-            path: "/admin/user-management",
-            element: <UserManagement/>
-        }, {
-            path: "/admin/order-management",
-            element: <OrderManagement/>
-        },
-        {
-            path: "/admin/material-management",
-            element: <MaterialManagement/>
-        }, {
-            path: "/admin/supplier-management",
-            element: <SupplierManagement/>,
-        },
-        {
-            path: "/admin/supplier-management/supplier",
-            element: <Supplier/>
-        },
-        {
-            path: "/menuItemInfo",
-            element: <MenuItemInfo/>
-        }
-    ]
-},])
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/order",
+        element: <Order />,
+      },
+      {
+        path: "/order-info",
+        element: <OrderInfo />,
+      },
+      {
+        path: "/admin",
+        element: <AdminPage />,
+      },
+      {
+        path: "/admin/menu-management",
+        element: <MenuManagement />,
+      },
+      {
+        path: "/admin/user-management",
+        element: <UserManagement />,
+      },
+      {
+        path: "/admin/order-management",
+        element: <OrderManagement />,
+      },
+      {
+        path: "/admin/material-management",
+        element: <MaterialManagement />,
+      },
+      {
+        path: "/admin/supplier-management",
+        element: <SupplierManagement />,
+      },
+      {
+        path: "/admin/supplier-management/supplier",
+        element: <Supplier />,
+      },
+      {
+        path: "/menuItemInfo",
+        element: <MenuItemInfo />,
+      },
+    ],
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <RecoilRoot> <RouterProvider router={router}/></RecoilRoot>
+  <RecoilRoot>
+    {" "}
+    <RouterProvider router={router} />
+  </RecoilRoot>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
