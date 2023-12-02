0fb7eb62223bb61e595e908252aac2e5efd3a6aa
SanghyeonLee <hyeon012366@gmail.com>
2023-12-02 14:03:00 +0900

    refactor: 관리자 페이지 레이아웃 및 링크 컴포넌트 구조 개선

--- src/pages/admin/AdminPage.jsx
+++ src/pages/admin/AdminPage.jsx
@@ -2,38 +2,58 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-function AdminPage(props) {
+function AdminPage() {
   return (
-    <>
-      <AdminTitleLayout>관리자 페이지</AdminTitleLayout>
-      <ul>
-        <PageLi>
-          <Link to={"/admin/menu-management"}>메뉴관리</Link>
-        </PageLi>
-        <PageLi>
-          <Link to={"/admin/user-management"}>고객관리</Link>
-        </PageLi>
-        <PageLi>
-          <Link to={"/admin/order-management"}>주문관리</Link>
-        </PageLi>
-        <PageLi>
-          <Link to={"/admin/material-management"}>재료관리</Link>
-        </PageLi>
-        <PageLi>
-          <Link to={"/admin/supplier-management"}>공급업체 목록</Link>
-        </PageLi>
-      </ul>
-    </>
+    <Container>
+      <Title>관리자 페이지</Title>
+      <List>
+        <li>
+          <StyledLink to="/admin/menu-management">메뉴관리</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/admin/user-management">고객관리</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/admin/order-management">주문관리</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/admin/material-management">재료관리</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/admin/supplier-management">공급업체 목록</StyledLink>
+        </li>
+      </List>
+    </Container>
   );
 }
 
-const AdminTitleLayout = styled.h2`
-  display: flex;
-  justify-content: center;
-`;
-const PageLi = styled.li`
-  display: flex;
-  justify-content: center;
+export default AdminPage;
+
+const Container = styled.div`
+  margin: 50px auto;
+  text-align: center;
 `;
 
-export default AdminPage;
+const Title = styled.h2`
+  font-size: 30px;
+  padding-bottom: 60px;
+  font-weight: bold;
+`;
+
+const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 18px;
+  color: #5e2ca5;
+  text-decoration: none;
+  font-weight: 500;
+
+  &:hover {
+    text-decoration: underline;
+    color: #4a2196;
+  }
+`;
