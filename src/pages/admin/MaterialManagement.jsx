9dc2906f545ed3b347c5271263e39ddb869b8b6b
SanghyeonLee <hyeon012366@gmail.com>
2023-12-03 09:26:00 +0900

    refactor: 재료 관리 페이지 상태 및 fetch 로직 개선

--- src/pages/admin/MaterialManagement.jsx
+++ src/pages/admin/MaterialManagement.jsx
@@ -1,138 +1,66 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import styled from "styled-components";
-import { CenterLayout } from "../../styles/CenterSortLayout";
+import { ButtonLayout } from "../../styles/ButtonLayout";
+import { useSetRecoilState } from "recoil";
+import { PopupAtom } from "../../recoil/PopupAtom";
+import { TableLayout } from "../../styles/TableLayout";
 
-function MaterialManagement(props) {
-  const [showMaterial, setShowMaterial] = useState([]);
-  const [popUp, setPopUp] = useState(false);
-  const [materialFormState, setMaterialFormState] = useState({
-    materialNum: "",
-    materialUnit: "",
-    materialName: "",
-    materialQuantity: "",
-  });
+function MaterialManagement() {
+  const [materials, setMaterials] = useState([]);
   const [trigger, setTrigger] = useState(false);
+  const setPopup = useSetRecoilState(PopupAtom);
 
   useEffect(() => {
-    const fetchMaterialInfo = async () => {
+    const fetchMaterials = async () => {
       try {
-        const result = await axios.get("http://localhost:8080/material/");
-        if (result.status === 200) {
-          //result.data.map((ele)=>{ele.materialUsage= ""})
-          setShowMaterial(result.data);
-          return;
-        }
-      } catch (err) {
+        const { data, status } = await axios.get(
+          "http://localhost:8080/admin/materials"
+        );
+        if (status === 200) setMaterials(data);
+      } catch {
         alert("재료를 불러오지 못했습니다.");
-        throw err;
       }
     };
-    fetchMaterialInfo();
+    fetchMaterials();
   }, [trigger]);
 
-  const inputFormChg = (e) => {
-    setMaterialFormState((prev) => ({
-      ...prev,
-      [e.target.name]: e.target.value,
-      materialNum: popUp.materialNum,
-    }));
-  };
-
-  const materialUdtBtn = async () => {
-    const result = await axios.put(
-      "http://localhost:8080/material/",
-      materialFormState
-    );
-    if (result.status === 200) {
-      alert("재료수정 성공");
-      setTrigger(!trigger);
-      return;
-    }
-    alert("수정 실패");
-  };
-
-  const popUpOpen = (ele) => {
-    setMaterialFormState({
-      materialNum: ele.materialNum,
-      materialUnit: ele.materialUnit,
-      materialName: ele.materialName,
-      materialQuantity: ele.materialQuantity,
+  const openPopup = (material) => {
+    setPopup({
+      popupType: "재료수정",
+      isOpen: true,
+      popupData: { ...material, setTrigger, trigger },
     });
-    setPopUp(ele);
   };
+
   return (
-    <>
-      {popUp && (
-        <MaterialPageLayout>
-          <h3>재료번호 : {popUp.materialNum}</h3>
-          재료단위 :{" "}
-          <input
-            type={"text"}
-            name={"materialUnit"}
-            onChange={inputFormChg}
-            defaultValue={popUp.materialUnit}
-          />
-          이름 :{" "}
-          <input
-            type={"text"}
-            name={"materialName"}
-            defaultValue={popUp.materialName}
-            onChange={inputFormChg}
-          />
-          수량 :{" "}
-          <input
-            type={"number"}
-            name={"materialQuantity"}
-            defaultValue={popUp.materialQuantity}
-            onChange={inputFormChg}
-          />
-          <button type={"button"} onClick={() => materialUdtBtn()}>
-            완료
-          </button>
-          <button type={"button"} onClick={() => setPopUp(false)}>
-            취소
-          </button>
-        </MaterialPageLayout>
-      )}
-      <CenterLayout>
-        <table border={"1"}>
-          <caption>
-            <h2>재료관리</h2>
-          </caption>
-          <thead>
-            <tr>
-              <th>항목번호</th>
-              <th>재료단위</th>
-              <th>이름</th>
-              <th>수량</th>
-              <th>수정</th>
-            </tr>
-          </thead>
-          <tbody>
-            {showMaterial.length > 0 &&
-              showMaterial.map((ele) => (
-                <tr key={ele.materialNum}>
-                  <td>{ele.materialNum}</td>
-                  <td>{ele.materialUnit}</td>
-                  <td>{ele.materialName}</td>
-                  <td>{ele.materialQuantity}</td>
-                  <td>
-                    <button type={"button"} onClick={() => popUpOpen(ele)}>
-                      수정
-                    </button>
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </CenterLayout>
-    </>
+    <TableLayout>
+      <caption>
+        <h2>재료관리</h2>
+      </caption>
+      <thead>
+        <tr>
+          <th>재료번호</th>
+          <th>단위</th>
+          <th>이름</th>
+          <th>수량</th>
+          <th>수정</th>
+        </tr>
+      </thead>
+      <tbody>
+        {materials.map((mat) => (
+          <tr key={mat.materialNum}>
+            <td>{mat.materialNum}</td>
+            <td>{mat.materialUnit}</td>
+            <td>{mat.materialName}</td>
+            <td>{mat.materialQuantity}</td>
+            <td>
+              <ButtonLayout onClick={() => openPopup(mat)}>수정</ButtonLayout>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </TableLayout>
   );
 }
-
-const MaterialPageLayout = styled.div`
-  text-align: center;
-`;
 
 export default MaterialManagement;
