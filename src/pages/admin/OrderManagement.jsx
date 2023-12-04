5dcd0a7db470838656afb0b48de299f57b96778d
SanghyeonLee <hyeon012366@gmail.com>
2023-12-04 11:27:00 +0900

    refactor: 주문 관리 페이지에서 사용자 ID 파라미터 제거 및 fetch 구조 정리

--- src/pages/admin/OrderManagement.jsx
+++ src/pages/admin/OrderManagement.jsx
@@ -1,65 +1,91 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useRecoilValue } from "recoil";
-import { UserIdAtom } from "../../recoil/UserAuth";
+import { TableLayout } from "../../styles/TableLayout";
+import { ButtonLayout } from "../../styles/ButtonLayout";
+import { useSetRecoilState } from "recoil";
+import { PopupAtom } from "../../recoil/PopupAtom";
 
-function OrderManagement(props) {
+function OrderManagement() {
   const [showMaterial, setShowMaterial] = useState([]);
-  const userIdValue = useRecoilValue(UserIdAtom);
+  const setPopup = useSetRecoilState(PopupAtom);
+
+  const formatDate = (isoString) => {
+    if (!isoString) return "";
+    return isoString.replace(
+      /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}).*/,
+      (_, y, m, d, h, min) => `${y}-${m}-${d} ${h}:${min}`
+    );
+  };
 
   useEffect(() => {
     const fetchOrderInfo = async () => {
-      const result = await axios.get("http://localhost:8080/admin/order", {
-        params: {
-          userId: userIdValue,
-        },
-      });
-      if (result.status === 200) {
-        //result.data.map((ele)=>{ele.materialUsage= ""})
-        setShowMaterial(result.data);
-      } else {
-        alert("재료를 불러오지 못했습니다.");
+      try {
+        const result = await axios.get("http://localhost:8080/admin/orders");
+        if (result.status === 200) {
+          setShowMaterial(result.data);
+        } else {
+          alert("주문를 불러오지 못했습니다.");
+        }
+      } catch (err) {
+        console.error(err);
+        alert("오류 발생");
       }
     };
     fetchOrderInfo();
   }, []);
-  console.log(showMaterial);
+
+  const handleDetailClick = (orderInfo) => {
+    setPopup({
+      isOpen: true,
+      popupType: "주문상세",
+      popupData: orderInfo,
+    });
+  };
+
   return (
-    <div>
-      <table border={"1"}>
-        <caption>
-          <h2>주문관리</h2>
-        </caption>
-        <thead>
+    <TableLayout>
+      <caption>
+        <h2>주문관리</h2>
+      </caption>
+      <thead>
+        <tr>
+          <th>주문번호</th>
+          <th>고객 ID</th>
+          <th>고객 이름</th>
+          <th>고객 연락처</th>
+          <th>주문 날짜</th>
+          <th>결제수단</th>
+          <th>주소</th>
+          <th>총 주문금액</th>
+          <th>주문 상세</th>
+        </tr>
+      </thead>
+      <tbody>
+        {showMaterial.length > 0 ? (
+          showMaterial.map((ele) => (
+            <tr key={ele.menuOrderNum}>
+              <td>{ele.menuOrderNum}</td>
+              <td>{ele.userId}</td>
+              <td>{ele.User?.userName}</td>
+              <td>{ele.User?.userPhoneNum}</td>
+              <td>{formatDate(ele.menuOrderDate)}</td>
+              <td>{ele.menuOrderPayment}</td>
+              <td>{ele.User?.userAddress}</td>
+              <td>{ele.menuOrderTotalPrice}</td>
+              <td>
+                <ButtonLayout onClick={() => handleDetailClick(ele)}>
+                  상세
+                </ButtonLayout>
+              </td>
+            </tr>
+          ))
+        ) : (
           <tr>
-            <th>주문번호</th>
-            <th>고객 ID</th>
-            <th>고객 이름</th>
-            <th>고객 연락처</th>
-            <th>주문 날짜</th>
-            <th>결제수단</th>
-            <th>배송지</th>
-            <th>총 주문금액</th>
+            <td colSpan={9}>주문이 없습니다</td>
           </tr>
-        </thead>
-        <tbody>
-          {showMaterial.length > 0
-            ? showMaterial.map((ele, idx) => (
-                <tr key={idx}>
-                  <td>{ele.menuOrderNum}</td>
-                  <td>{ele.userId}</td>
-                  <td>{ele["User.userName"]}</td>
-                  <td>{ele["User.userPhoneNum"]}</td>
-                  <td>{ele.menuOrderDate}</td>
-                  <td>{ele.menuOrderPayment}</td>
-                  <td>{ele["User.userAddress"]}</td>
-                  <td>{ele.menuOrderTotalPrice}</td>
-                </tr>
-              ))
-            : null}
-        </tbody>
-      </table>
-    </div>
+        )}
+      </tbody>
+    </TableLayout>
   );
 }
 
