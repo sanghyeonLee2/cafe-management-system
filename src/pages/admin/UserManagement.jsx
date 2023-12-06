c3d84356d9dc3e00b540280c43d602337f4aebec
SanghyeonLee <hyeon012366@gmail.com>
2023-12-06 13:10:00 +0900

    refactor: 고객 관리 페이지 TableLayout 적용

--- src/pages/admin/UserManagement.jsx
+++ src/pages/admin/UserManagement.jsx
@@ -1,51 +1,48 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { CenterLayout } from "../../styles/CenterSortLayout";
+import { TableLayout } from "../../styles/TableLayout";
 
-function MaterialManagement(props) {
+function UserManagement(props) {
   const [showUserInfo, setShowUserInfo] = useState([]);
 
   useEffect(() => {
     const fetchMaterialInfo = async () => {
-      const result = await axios.get("http://localhost:8080/admin/");
+      const result = await axios.get("http://localhost:8080/admin/users");
       if (result.status === 200) {
-        //result.data.map((ele)=>{ele.materialUsage= ""})
         setShowUserInfo(result.data);
       } else {
-        alert("재료를 불러오지 못했습니다.");
+        alert("고객을 불러오지 못했습니다.");
       }
     };
     fetchMaterialInfo();
   }, []);
   return (
-    <CenterLayout>
-      <table border={"1"}>
-        <caption>
-          <h2>고객관리</h2>
-        </caption>
-        <thead>
-          <tr>
-            <th>고객아이디</th>
-            <th>고객주소</th>
-            <th>고객이름</th>
-            <th>고객전화번호</th>
-          </tr>
-        </thead>
-        <tbody>
-          {showUserInfo.length > 0
-            ? showUserInfo.map((ele, idx) => (
-                <tr key={idx}>
-                  <td>{ele.userId}</td>
-                  <td>{ele.userAddress}</td>
-                  <td>{ele.userName}</td>
-                  <td>{ele.userPhoneNum}</td>
-                </tr>
-              ))
-            : null}
-        </tbody>
-      </table>
-    </CenterLayout>
+    <TableLayout border={"1"}>
+      <caption>
+        <h2>고객관리</h2>
+      </caption>
+      <thead>
+        <tr>
+          <th>고객아이디</th>
+          <th>고객주소</th>
+          <th>고객이름</th>
+          <th>고객전화번호</th>
+        </tr>
+      </thead>
+      <tbody>
+        {showUserInfo.length > 0
+          ? showUserInfo.map((ele, idx) => (
+              <tr key={idx}>
+                <td>{ele.userId}</td>
+                <td>{ele.userAddress}</td>
+                <td>{ele.userName}</td>
+                <td>{ele.userPhoneNum}</td>
+              </tr>
+            ))
+          : null}
+      </tbody>
+    </TableLayout>
   );
 }
 
-export default MaterialManagement;
+export default UserManagement;
