116a2dbec2e1aa4659061c94102a576e3472f02b
SanghyeonLee <hyeon012366@gmail.com>
2023-12-07 13:15:00 +0900

    feat: 주문 목록 상태에 총 주문 금액 selector 추가

--- src/recoil/OrderListAtom.js
+++ src/recoil/OrderListAtom.js
@@ -1,5 +1,20 @@
-import {atom} from "recoil";
+import { atom } from "recoil";
+import { selector } from "recoil";
+
 export const OrderListAtom = atom({
-    key:"OrderListAtom",
-    default : []
-})
+  key: "OrderListAtom",
+  default: [],
+});
+
+export const TotalOrderPriceSelector = selector({
+  key: "TotalOrderPriceSelector",
+  get: ({ get }) => {
+    const orderList = get(OrderListAtom);
+
+    if (!Array.isArray(orderList) || orderList.length === 0) return 0;
+
+    return orderList.reduce((acc, item) => {
+      return acc + item.menuItemPrice * item.orderDetailQuantity;
+    }, 0);
+  },
+});
